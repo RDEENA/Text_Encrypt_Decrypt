a96106a51f19a5ab79f677011045f84f847530b7
RDEENA <85985255+RDEENA@users.noreply.github.com>
2023-11-14 14:09:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,29 @@
 import time
 import streamlit as st
 import streamlit.components.v1 as components
-#components.html('''<html><body><img src="upchat_technologies_logo.jpg"></body><html>''')
-#st.image("upchat_technologies_logo.jpg")
+def Caesar_Cipher(inp,val):
+    enstr = ""
+    for i in inp:
+        if 'A' <= i <= 'Z':
+            enstr += chr(((ord(i) - ord('A') + val) % 26) + ord('A'))
+        elif 'a' <= i <= 'z':
+            enstr += chr(((ord(i) - ord('a') + val) % 26) + ord('a'))
+        else:
+            enstr += i
+    return enstr
+def User_Interface(str1, str2):
+    with st.spinner(text="Please Wait", cache=True):
+        time.sleep(2)
+    with st.status(f"{str1} data"):
+        time.sleep(2)
+        st.write(f"{str2} done")
+    pg = st.progress(0)
+    for i in range(101):
+        pg.progress(i)
+        time.sleep(0.01)
+    st.success(f"{str2} done :smiley:")
 flag=0
 components.html('''<style> body{padding:20px;}</style><body bgcolor = "powderblue"><h1><center><b>Caesar Cipher</h1></body>''')
-#st.markdown("""<style>body { background-image: url('upchat_technologies_logo.jpg'); background-size: cover;}</style>""", unsafe_allow_html=True)
-#st.header("Caesar Cipher")
-st.subheader("Internship project")
 st1 = st.file_uploader("Click here to upload file",type="txt")
 if st1 is not None:
     f_open = st1.read()
@@ -29,51 +45,13 @@ elif opt== "Encrypt":
      btn = st.button("Click to Encrypt")
      if btn == True:
         flag =2
-if flag== 2:
-   with st.spinner(text="Please Wait",cache=True):
-        time.sleep(2)
-   with st.status("Encrypting data"):
-           time.sleep(2)
-           st.write("Encryption done")
-   pg = st.progress(0)
-   for i in range(101):
-       pg.progress(i)
-       time.sleep(0.01)
-
-
-   st.success("Encryption done :smiley:")
-   enstr = ""
-   for i in inp:
-       if 'A' <= i <= 'Z':
-           enstr += chr(((ord(i) - ord('A') + val) % 26) + ord('A'))
-       elif 'a' <= i <= 'z':
-           enstr += chr(((ord(i) - ord('a') + val) % 26) + ord('a'))
-       else:
-           enstr += i
-   st.code(enstr)
-
-   st.download_button(
-           label="Download Encrypted File :open_file_folder:",
-           data=enstr,
-           file_name="Encrypted.txt", help="Click to download")
+if  flag== 2:
+    User_Interface("Encrypting","Encryption")
+    res = Caesar_Cipher(inp,val)
+    st.code(res)
+    st.download_button(label="Download Encrypted File :open_file_folder:",data=res,file_name="Encrypted.txt", help="Click to download")
 elif flag==1:
-    with st.spinner(text="Please Wait", cache=True):
-        time.sleep(2)
-    with st.status("Decrypting data"):
-        time.sleep(2)
-        st.write("Decryption done")
-    pg = st.progress(0)
-    for i in range(101):
-        pg.progress(i)
-        time.sleep(0.01)
-    st.success("Decryption done :smiley:")
-    enstr = ""
-    for i in inp:
-        if 'A' <= i <= 'Z':
-            enstr += chr(((ord(i) - ord('A') + val1) % 26) + ord('A'))
-        elif 'a' <= i <= 'z':
-            enstr += chr(((ord(i) - ord('a') + val1) % 26) + ord('a'))
-        else:
-            enstr += i
-    st.code(enstr)
-    st.download_button(label="Download Decrypted File", data= enstr, help="Click here to download",file_name="Decrypted.txt")
+     User_Interface("Decrypting","Decryption")
+     res = Caesar_Cipher(inp,val1)
+     st.code(res)
+     st.download_button(label="Download Decrypted File :open_file_folder:", data=res, file_name="Decrypted.txt",help="Click to download")
